2607dc9bd7cb00822333c1693f7329e32623060a
shane <qaz56115624@gmail.com>
2022-01-18 10:50:37 +0800

    ok

--- select_data.py
+++ select_data.py
@@ -3,7 +3,7 @@ import mysql.connector
 connection = mysql.connector.connect(host='localhost',
                                     port='3306',
                                     user='root',
-                                    password='az30113011',
+                                    password='password',
                                     database='company'
                                     )
 
